90176109bdf7935662a324daee37b7c27dcc0b07
Jannis Limperg <jannis.limperg@arcor.de>
2011-10-30 23:56:54 +0100

    YARD setup
    YARD now uses markdown for markup processing, and the existing
    documentation has been modified accordingly. Moreover, a Rake
    task for YARD documentation has been added.

--- Rakefile
+++ Rakefile
@@ -1,3 +1,7 @@
 # RSpec task
 require 'rspec/core/rake_task'
 RSpec::Core::RakeTask.new(:spec)
+
+# YARD task
+require 'yard'
+YARD::Rake::YardocTask.new(:doc)
--- lib/style.rb
+++ lib/style.rb
@@ -10,13 +10,14 @@ module RCite
     end
 
     # Generates a citation for the given `text`. This method dynamically
-    # looks up the method `cite_#!{text[:type]}` and calls it with
+    # looks up the method `"cite_#{text[:type]}"` and calls it with
     # `text` as the only argument.
     #
     # @param [Hash] text A bibliography entry hash
     #   in the 'citeproc' format as returned by `BibTeX::Entry#to_citeproc`
     # @raise ArgumentError if the style does not support the given entry's
     #   `type`.
+    # @return [String] The citation.
     def cite(text)
       method = "cite_#{text[:type]}".to_s
       if !respond_to?(method)
@@ -27,11 +28,12 @@ module RCite
     end
     
     # Generates a bibliography entry for the given `text`. This method dynamically
-    # looks up the method `bib_#!{text[:type]}` and calls it with
+    # looks up the method `"bib_#{text[:type]}"` and calls it with
     # `text` as the only argument.
     #
     # @param (see #cite)
     # @raise (see #cite)
+    # @return [String] The bibliography entry.
     def bib(text)
       method = "bib_#{text[:type]}".to_s
       if !respond_to?(method)

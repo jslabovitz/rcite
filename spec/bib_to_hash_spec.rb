b4e19882d2220b81314d195c2811330bce6fd817
Jannis Limperg <jannis.limperg@arcor.de>
2011-11-01 17:36:44 +0100

    Library compatibility spec for bibtex-ruby
    In order to ensure that future versions of the
    bibtex-ruby gem do not introduce API changes that
    break compatibility, this commit adds a spec that
    compares a hash constructed by bibtex-ruby from
    a .bib file to the hash I expect.
    
    This is also practical for visualizing how bibtex-ruby
    converts BibTeX values to citeproc format, especially
    for names and dates.

---
+++ spec/bib_to_hash_spec.rb
@@ -0,0 +1,19 @@
+require 'yaml'
+require 'bibtex'
+
+ALL_TYPES_BIB_FILE = 'spec/files/all_types.bib'
+ALL_TYPES_YML_FILE = 'spec/files/all_types.yml'
+
+# This spec exists solely to ensure that there are no API changes
+# in bibtex-ruby that I don't realize.
+
+describe "The translation from bib to hash" do
+  it "should be persistent" do
+    bib = BibTeX::Bibliography.open(ALL_TYPES_BIB_FILE)
+    yaml = ""
+    File.open(ALL_TYPES_YML_FILE, "r") do |f|
+      f.each { yaml << f.read }
+    end
+    bib.to_citeproc.should == YAML.load(yaml).to_a
+  end
+end

4f0b6f45ff44f6d18b6e7ed34514babcd340e9f4
Jannis Limperg <jannis.limperg@arcor.de>
2012-04-06 14:11:10 +0200

    Central simplecov configuration
    Adds a central simplecov configuration for Cucumber and rspec in
    favour of identical configurations in spec_helper.rb and env.rb.
    
    Moreover excludes features and spec files from coverage results
    as we're only interested in the coverage of the actual RCite
    code.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,19 +1,6 @@
 # SimpleCov Configuration
 require 'simplecov'
 
-SimpleCov.at_exit do
-  SimpleCov.result.format!
-  File.open(File.join(SimpleCov.coverage_path, 'percentage.txt'), 'w') do |f|
-    f.write(SimpleCov.result.covered_percent)
-  end
-end
-
-SimpleCov.configure do
-  add_filter 'styles/'
-end
-
-SimpleCov.start
-
 # RSpec Setup
 
 require 'rspec/expectations'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,22 +3,10 @@ $: << './lib/'
 $: << './lib/rcite/'
 $: << './styles/'
 
-# SimpleCov configuration
+# SimpleCov start
 require 'simplecov'
 
-SimpleCov.at_exit do
-  SimpleCov.result.format!
-  File.open(File.join(SimpleCov.coverage_path, 'percentage.txt'), 'w') do |f|
-    f.write(SimpleCov.result.covered_percent)
-  end
-end
-
-SimpleCov.configure do
-  add_filter 'styles/'
-end
-
-SimpleCov.start
-
+# Helper methods
 def spec_text(text, result)
   @pro.cite(text).should == result
 end
